50d99790906ac4a74f3c7757faf1af2768a3319b
Vabank <ermakov.pavel@gmail.com>
2021-04-08 15:43:13 +0300

    fix

--- public/local/src/Content/User.php
+++ public/local/src/Content/User.php
@@ -37,6 +37,9 @@ class User
                     $arUser["pathPersonalPhoto"] = $v;
                 }
             }
+            if (empty($arUser["pathPersonalPhoto"])) {
+                $arUser["pathPersonalPhoto"] = DEFAULT_AVATAR_USER;
+            }
         }
         if (empty($arUser["pathPersonalPhoto"])) {
             $arUser["pathPersonalPhoto"] = "/img/avatar-default.png";
