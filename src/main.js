3aac82442bf441e5a8d52329ba2c42f0f32a51e0
Vabank <ermakov.pavel@gmail.com>
2021-04-08 17:30:01 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import InfiniteLoading from 'vue-infinite-loading'
 // import CoolLightBox from 'vue-cool-lightbox'
 // import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
 import VuePictureSwipe from 'vue-picture-swipe'
-import 'photoswipe/dist/default-skin/default-skin.css'
 
 Vue.config.productionTip = false
 Vue.use(helper)
