6c2f4d3c49245669e70e324f476d677744da2f95
Vabank <ermakov.pavel@gmail.com>
2021-04-08 15:44:33 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-global $APPLICATION;
+$APPLICATION->SetTitle("Pavel V. Ermakov");
 \Vabank\API\CssJsVue::On();
-$arUser = \Vabank\API\UsersExt::UserGetById(DEFAULT_USER_ID);
-$APPLICATION->SetTitle("".$arUser["NAME"]." ".$arUser["SECOND_NAME"].". ".$arUser["LAST_NAME"]."");
 ?>
 <div id="app"></div>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
